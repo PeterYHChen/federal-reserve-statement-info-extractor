b1db2dab40ea0948d22dd700f4a37ce4dae969f6
Jeremy James Lao <jeremylao12@yahoo.co.uk>
2017-04-10 23:59:08 -0400

    java & python utilities

--- parsefile.java
+++ parsefile.java
@@ -21,15 +21,20 @@ public class parsefile{
 		if(length_string > 0){
 		    for(int i = 0; i< length_string; ++i){
 			
-			if(line_list[i].equals("EDT") || line_list[i].equals("EST")){
+			if(line_list[i].equals("EDT") || line_list[i].equals("EST") || line_list[i].equals("release")  ){
 			    on_flag = 1;
+			    
 			}
-			if(on_flag == 1 && line_list[i].equals("Implementation") && line_list[i+1].equals("Note")){
+			if(on_flag == 1 && line_list[i].equals("Implementation") && line_list[i+1].equals("Note") ){
 			    on_flag = 0;
 			}
-			if(on_flag == 1 && !line_list[i].equals("") && !line.isEmpty() && (!line_list[i].equals("EST") || !line_list[i].equals("EDT")) && !line_list[i].equals("Share")){
+			if(on_flag == 1 && line_list[i].equals("Last") && line_list[i+1].equals("Update:")){
+			    on_flag = 0;
+			}
+
+			if(on_flag == 1 && !line_list[i].equals("") && !line.isEmpty() && !line_list[i].equals("release") && !line_list[i].equals("EST") && !line_list[i].equals("EDT") && !line_list[i].equals("Share")){
 			    int string_len = line_list[i].length();
-			    if(string_len > 0 && (line_list[i].charAt(string_len-1) == '.' || line_list[i].charAt(string_len-1) == ',' || line_list[i].charAt(string_len-1) == ';')){
+			    if(string_len > 0 && (line_list[i].charAt(string_len-1) == '.' || line_list[i].charAt(string_len-1) == ',' || line_list[i].charAt(string_len-1) == ':'  || line_list[i].charAt(string_len-1) == ';')){
 				if(line_list[i].charAt(string_len-1) == '.' && string_len>2){
 				    System.out.println(line_list[i].substring(0,string_len-1));
 				    System.out.println(line_list[i].charAt(string_len-1));
