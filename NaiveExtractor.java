e6fd904474cba37b86b3468805f42a72669c4af2
yonghong <yonghong.chenn@gmail.com>
2017-04-19 14:43:54 -0400

    add function of extracting voting names each month

--- NaiveExtractor.java
+++ NaiveExtractor.java
@@ -22,25 +22,23 @@ public class NaiveExtractor {
     private static void extract(String directoryPath) throws IOException {
         File directory = new File(directoryPath);
         List<String> results = new ArrayList<>();
-
+        List<String> other = new ArrayList<>();
+        Map<String, List<List<String>>> fileContents = new TreeMap<>();
         File[] files = directory.listFiles();
         Arrays.sort(files);
         for (int i = 0; i < files.length; i++) {
             File statement = files[i];
             List<String> lines = Files.readAllLines(statement.toPath(), StandardCharsets.UTF_8);
             List<String> words = new ArrayList<>();
+            List<List<String>> sentences = new ArrayList<>();
             String fileName = statement.getName().substring(0, statement.getName().indexOf("."));
             System.out.println("Processing file " + fileName);
-            String sentence;
 
             for (int j = 0; j < lines.size(); j++) {
                 String word = lines.get(j).trim();
                 if (word.equals("")) {
-                    // process words
-                    sentence = formSentence(words);
-                    if (!sentence.isEmpty())
-                        results.add(fileName + "\t" + sentence);
-                    words.clear();
+                    sentences.add(words);
+                    words = new ArrayList<>();
                 } else {
                     words.add(word);
                 }
@@ -48,18 +46,163 @@ public class NaiveExtractor {
 
             // process the last sentence of words
             if (!words.isEmpty()) {
-                sentence = formSentence(words);
-                if (!sentence.isEmpty())
-                    results.add(fileName + "\t" + sentence);
+                sentences.add(words);
             }
 
+            fileContents.put(fileName, sentences);
+            results.addAll(extractFundsRate(fileName, sentences));
             results.add("");
         }
 
+        writeTofiles(results, "federalFundsRate.txt");
+        extractVotingNames(fileContents, "votingNames.txt");
+    }
+
+    private static void extractVotingNames(Map<String, List<List<String>>> files, String outputFile) {
+        Map<String, HashSet<String>> votingNamesMap = new TreeMap<>();
+        Set<String> uniqueVotingNames = new HashSet<>();
+
+        for (String fileName : files.keySet()) {
+            votingNamesMap.put(fileName, new HashSet<>());
+
+            List<List<String>> sentences = files.get(fileName);
+            for (List<String> words : sentences) {
+                boolean isTargetSentence = false;
+                boolean hasNumbers = false;
+                StringBuffer result = new StringBuffer();
+                StringBuffer sentence = new StringBuffer();
+                List<String> votingNames = new ArrayList<>();
+
+                for (int i = 0; i < words.size(); i++) {
+                    if (!words.get(i).matches("[^A-Za-z0-9]"))
+                        sentence.append(" ");
+                    sentence.append(words.get(i));
+
+                    if (!isTargetSentence && i + 1 < words.size()) {
+                        if (words.get(i).equals("Voting") && words.get(i+1).equals("for")) {
+                            isTargetSentence = true;
+                        }
+                    }
+                }
+
+                if (isTargetSentence) {
+                    String nameSentence = sentence.substring(sentence.indexOf(":") + 1).trim();
+                    String[] names = nameSentence.split(";");
+                    for (String name : names) {
+                        name = name.split(",")[0].trim();
+                        votingNames.add(name);
+                    }
+                    // the last name pattern of the sentence is "and [name].", should remove it
+                    String name = votingNames.get(votingNames.size()-1);
+                    name = name.substring(name.indexOf(' ') + 1, name.length() - 1);
+                    votingNames.set(votingNames.size() - 1, name);
+                }
+                votingNamesMap.get(fileName).addAll(votingNames);
+            }
+            uniqueVotingNames.addAll(votingNamesMap.get(fileName));
+        }
+
+        List<String> results = formatVotingNameData(uniqueVotingNames, votingNamesMap);
+        writeTofiles(results, outputFile);
+    }
+
+    private static List<String> formatVotingNameData(Set<String> uniqueVotingNames, Map<String, HashSet<String>> votingNamesMap) {
+        List<String> results = new ArrayList<>();
+        List<String> votingNames = new ArrayList<>();
+        votingNames.addAll(uniqueVotingNames);
+
+        StringBuffer result = new StringBuffer();
+        result.append("[");
+        for (String name : votingNames) {
+            result.append("'").append(name).append("'").append(", ");
+        }
+        result.append("{ role: 'annotation' } ],");
+        results.add(result.toString());
+
+        for (String fileName : votingNamesMap.keySet()) {
+            result = new StringBuffer();
+            result.append("['").append(fileName).append("', ");
+            HashSet<String> namesInEachFile = votingNamesMap.get(fileName);
+
+            for (String name : votingNames) {
+                if (namesInEachFile.contains(name))
+                    result.append("1, ");
+                else
+                    result.append("0, ");
+            }
+            result.append("''],");
+
+            results.add(result.toString());
+        }
+
+        return results;
+    }
+    private static List<String> extractFundsRate(String fileName, List<List<String>> sentences) {
+        List<String> results = new ArrayList<>();
+
+        for (List<String> words : sentences) {
+            boolean isTargetSentence = false;
+            boolean hasNumbers = false;
+            StringBuffer result = new StringBuffer();
+            StringBuffer sentence = new StringBuffer();
+            String keyword = "";
+            String rangeBegin = "";
+            String rangeEnd = "";
+
+            for (int i = 0; i < words.size(); i++) {
+                sentence.append(" " + words.get(i));
+
+                if (!isTargetSentence && i + 2 < words.size()) {
+                    if (words.get(i).equals("federal") && words.get(i+1).equals("funds") && words.get(i+2).equals("rate")) {
+                        keyword = words.get(i) + " " + words.get(i + 1) + " " + words.get(i + 2);
+                        isTargetSentence = true;
+                    }
+                }
+
+                if (!hasNumbers && i + 2 < words.size()) {
+                    if (isNumber(words.get(i)) && words.get(i+1).equals("to") && isNumber(words.get(i+2))) {
+                        rangeBegin = words.get(i);
+                        rangeEnd = words.get(i+2);
+                        hasNumbers = true;
+                    }
+                }
+            }
+
+            Set<String> uniqueWords = new HashSet<>();
+            uniqueWords.addAll(words);
+            // ensure Committee perform this action
+            if (isTargetSentence && hasNumbers && uniqueWords.contains("Committee")) {
+                result.append(formatFederalFundsRateOutput(fileName, rangeBegin, rangeEnd));
+            }
+
+            if (!result.toString().isEmpty()) {
+                results.add(result.toString());
+            }
+        }
+
+        return results;
+    }
+
+    // form output to show graph in Google Chart API
+    private static String formatFederalFundsRateOutput(String fileName, String rangeBegin, String rangeEnd) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("['").append(fileName).append("', ")
+                .append(rangeBegin).append(", ")
+                .append(rangeBegin).append(", ")
+                .append(rangeEnd).append(", ")
+                .append(rangeEnd).append("],");
+        return sb.toString();
+    }
+
+    private static boolean isNumber(String value) {
+        return value.matches("(\\d+[/]\\d+)|(\\d+)");
+    }
+
+    private static void writeTofiles (List<String> results, String fileName) {
         // print results to file
         PrintWriter pw = null;
         try {
-            pw = new PrintWriter(new FileWriter("federalFundsRate.txt", false));
+            pw = new PrintWriter(new FileWriter(fileName, false));
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -71,49 +214,4 @@ public class NaiveExtractor {
         pw.append(sb.toString());
         pw.close();
     }
-
-    private static String formSentence(List<String> words) {
-        boolean isTargetSentence = false;
-        boolean hasNumbers = false;
-        StringBuffer result = new StringBuffer();
-        StringBuffer sentence = new StringBuffer();
-        String keyword = "";
-        String values = "";
-
-        for (int i = 0; i < words.size(); i++) {
-            sentence.append(" " + words.get(i));
-
-            if (!isTargetSentence && i + 2 < words.size()) {
-                if (words.get(i).equals("federal") && words.get(i+1).equals("funds") && words.get(i+2).equals("rate")) {
-                    keyword = words.get(i) + " " + words.get(i + 1) + " " + words.get(i + 2);
-                    isTargetSentence = true;
-                }
-            }
-
-            if (!hasNumbers && i + 2 < words.size()) {
-                if (isNumber(words.get(i)) && words.get(i+1).equals("to") && isNumber(words.get(i+2))) {
-//                    values = words.get(i) + " " + words.get(i + 1) + " " + words.get(i + 2);
-                    values = words.get(i) + "\t" + words.get(i) + "\t" + words.get(i+2) + "\t" + words.get(i+2);
-
-                    hasNumbers = true;
-                }
-            }
-        }
-
-        Set<String> uniqueWords = new HashSet<>();
-        uniqueWords.addAll(words);
-        // ensure Committee perform this action
-        if (isTargetSentence && hasNumbers && words.contains("Committee")) {
-            result.append(values);
-//            result.append(keyword + ": " + values);
-//            result.append("\n" + sentence.toString());
-        }
-
-        return result.toString();
-    }
-
-    private static boolean isNumber(String value) {
-        return value.matches("(\\d+[/]\\d+)|(\\d+)");
-    }
-
 }
